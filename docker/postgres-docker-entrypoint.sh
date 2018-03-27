5351268dbcb8ae3bf54968f2f2e4ff1f4ed3fd6c
Michael Schubert <michael@kinvolk.io>
2018-03-27 17:23:53 +0200

    postgres-docker-entrypoint: TLS and clientcert auth config

--- docker/postgres-docker-entrypoint.sh
+++ docker/postgres-docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -ex
+
 mkdir -p "/run/postgresql"
 chown -R postgres:postgres "/run/postgresql"
 
@@ -8,13 +10,27 @@ if [ ! -f "$PGDATA/postgresql.conf" ]; then
     chown -R postgres:postgres "$PGDATA"
 
     gosu postgres initdb
-    sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
+    # listen on all addresses
+    sed -ri "s|^#(listen_addresses\s*=\s*)\S+|\1'*'|" "$PGDATA"/postgresql.conf
 
-    # WARNING! This is not secure as it allows anyone to connect to your database
-    # remotely without asking for any password. This is just an example, please
-    # consider adding a user to the database, use md5 instead of trust and, if 
-    # needed, restrict the network range that should be allowed to connect.
-    { echo; echo "host all all 0.0.0.0/0 trust"; } >> "$PGDATA/pg_hba.conf"
+    # setup ssl: https://www.postgresql.org/docs/10/static/ssl-tcp.html
+    if [ -f "$PGTLS/server.crt" ]; then
+      sed -ri "s|^#(ssl\s*=\s*)\S+|\1on|" "$PGDATA"/postgresql.conf
+      sed -ri "s|^#(ssl_cert_file\s*=\s*)\S+|\1'$PGTLS/server.crt'|" "$PGDATA"/postgresql.conf
+      sed -ri "s|^#(ssl_key_file\s*=\s*)\S+|\1'$PGTLS/server.key'|" "$PGDATA"/postgresql.conf
+
+      # only allow clients when clientcert is enabled and provided:
+      # https://www.postgresql.org/docs/10/static/auth-pg-hba-conf.html
+      if [ -f "$PGTLS/ca.crt" ]; then
+        sed -ri "s|^#(ssl_ca_file\s*=\s*)\S+|\1'$PGTLS/ca.crt'|" "$PGDATA"/postgresql.conf
+
+        echo "hostssl all all 0.0.0.0/0 cert clientcert=1" >> "$PGDATA/pg_hba.conf"
+      else
+        echo "no trusted certificate authority given, cannot enable client certs" >&2
+      fi
+    else
+      echo "unable to enable ssl, no server.crt file found" >&2
+    fi
 
     gosu postgres pg_ctl -w start
 
