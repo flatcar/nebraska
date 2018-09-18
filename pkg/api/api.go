b104f8cf1678048f646654d8e10d94cb59cce2de
Krzesimir Nowak <krzesimir@kinvolk.io>
2018-09-18 11:05:57 +0200

    api: Add some API constructor for tests
    The constructor makes sure that the database is populated with sample
    data. Some tests rely on the data to exist in the database.

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -104,3 +104,24 @@ func OptionInitDB(api *API) error {
 func (api *API) Close() {
 	_ = api.db.DB.Close()
 }
+
+// NewForTest creates a new API instance with given options and fills
+// the database with sample data for testing purposes.
+func NewForTest(options ...func(*API) error) (*API, error) {
+	a, err := New(options...)
+	if err != nil {
+		return nil, err
+	}
+
+	sqlFile, err := Asset("db/sample_data.sql")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = a.db.Exec(string(sqlFile))
+	if err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
