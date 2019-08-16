508582491967c8327d1da61e0beeb61ca1f16ee0
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-16 13:18:28 +0100

    frontend: Port Channels list to Material UI

--- frontend/src/js/components/Channels/List.react.js
+++ frontend/src/js/components/Channels/List.react.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import { applicationsStore } from "../../stores/Stores"
 import React from "react"
-import { Row, Col } from "react-bootstrap"
 import _ from "underscore"
 import ModalButton from "../Common/ModalButton.react"
 import ModalUpdate from "./ModalUpdate.react"
 import Item from "./Item.react"
 import Loader from "react-spinners/ScaleLoader"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 class List extends React.Component {
 
@@ -69,29 +72,30 @@ class List extends React.Component {
     const channelToUpdate =  !_.isEmpty(channels) && this.state.updateChannelIDModal ? _.findWhere(channels, {id: this.state.updateChannelIDModal}) : null
 
     return (
-      <div>
-        <Row>
-          <Col xs={12}>
-            <h1 className="displayInline mainTitle">Channels</h1>
-            <ModalButton
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant="h4" className="displayInline mainTitle">Channels</Typography>
+          <ModalButton
               icon="plus"
               modalToOpen="AddChannelModal"
               data={{packages: packages, applicationID: this.props.appID}} />
-          </Col>
-        </Row>
-        <div className="groups--channelsList">
-          {entries}
-        </div>
-        {/* Update channel modal */}
-        {channelToUpdate &&
-          <ModalUpdate
-            data={{packages: packages, applicationID: this.props.appID, channel: channelToUpdate}}
-            modalVisible={this.state.updateChannelModalVisible}
-            onHide={this.closeUpdateChannelModal} />
-        }
-      </div>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent className="groups--packagesList">
+              {entries}
+              {/* Update channel modal */}
+              {channelToUpdate &&
+                <ModalUpdate
+                  data={{packages: packages, applicationID: this.props.appID, channel: channelToUpdate}}
+                  modalVisible={this.state.updateChannelModalVisible}
+                  onHide={this.closeUpdateChannelModal} />
+              }
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     )
-
   }
 }
 
