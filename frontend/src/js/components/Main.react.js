39e3212488c7ec1c9c34e75c19314ae4935ef23a
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-16 15:59:16 +0100

    frontend: Add a theme provider
    The theme provider will make it simpler to tweak the theme once we
    get to that phase, but the reason for adding it now is that this way
    we can quickly change a few global styling settings rather than having
    to add them later.

--- frontend/src/js/components/Main.react.js
+++ frontend/src/js/components/Main.react.js
@@ -5,17 +5,33 @@ import ProgressBar from "./ProgressBar.react"
 import MainLayout from "./Layouts/MainLayout.react"
 import ApplicationLayout from "./Layouts/ApplicationLayout.react"
 import GroupLayout from "./Layouts/GroupLayout.react"
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
+const nebraskaTheme = createMuiTheme({
+  palette: {
+    primary: {
+      contrastText: "#fff",
+      main: '#00AEEF',
+    },
+  },
+  typography: {
+    // Account for base font-size of 62.5%.
+    htmlFontSize: 10,
+  },
+});
 
 export default function Main() {
   return (
-    <Header />
-    <ProgressBar name="main_progress_bar" color="#ddd" width={0.2} />
-    <Switch>
-      <Route path='/' exact component={MainLayout} />
-      <Route path='/apps' exact component={MainLayout} />
-      <Route path="/apps/:appID" exact component={ApplicationLayout} />
-      <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
-    </Switch>
+    <ThemeProvider theme={nebraskaTheme}>
+      <Header />
+      <ProgressBar name="main_progress_bar" color="#ddd" width={0.2} />
+      <Switch>
+        <Route path='/' exact component={MainLayout} />
+        <Route path='/apps' exact component={MainLayout} />
+        <Route path="/apps/:appID" exact component={ApplicationLayout} />
+        <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
+      </Switch>
+    </ThemeProvider>
   );
 }
