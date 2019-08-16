9aeb555032b16b6836ed3d84ef458fc9ce02e5cb
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-16 15:57:28 +0100

    frontend: Refactor Main
    This patch refactors how the Main component is created by changing it
    from a component extension to a function (reducing a lot of
    unnecessary code).

--- frontend/src/js/components/Main.react.js
+++ frontend/src/js/components/Main.react.js
@@ -1,32 +1,21 @@
-import React, { PropTypes } from "react"
-import {Switch, Route, RouteHandler } from "react-router-dom"
+import React from "react"
+import {Switch, Route } from "react-router-dom"
 import Header from "./Header.react"
 import ProgressBar from "./ProgressBar.react"
 import MainLayout from "./Layouts/MainLayout.react"
 import ApplicationLayout from "./Layouts/ApplicationLayout.react"
 import GroupLayout from "./Layouts/GroupLayout.react"
 
-class Main extends React.Component {
 
-  constructor() {
-    super()
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <ProgressBar name="main_progress_bar" color="#ddd" width={0.2} />
-        <Switch>
-          <Route path='/' exact component={MainLayout} />
-          <Route path='/apps' exact component={MainLayout} />
-          <Route path="/apps/:appID" exact component={ApplicationLayout} />
-          <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
-        </Switch>
-      </div>
-    )
-  }
-
+export default function Main() {
+  return (
+    <Header />
+    <ProgressBar name="main_progress_bar" color="#ddd" width={0.2} />
+    <Switch>
+      <Route path='/' exact component={MainLayout} />
+      <Route path='/apps' exact component={MainLayout} />
+      <Route path="/apps/:appID" exact component={ApplicationLayout} />
+      <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
+    </Switch>
+  );
 }
-
-export default Main
