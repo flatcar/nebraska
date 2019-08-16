2f8a260242f8c7ec252d312f0e13471590bc97d0
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-16 13:13:20 +0100

    frontend: Port Packages list to Material UI

--- frontend/src/js/components/Packages/List.react.js
+++ frontend/src/js/components/Packages/List.react.js
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 import { applicationsStore } from "../../stores/Stores"
 import React from "react"
-import { Row, Col } from "react-bootstrap"
 import _ from "underscore"
 import ModalButton from "../Common/ModalButton.react"
 import Item from "./Item.react"
 import ModalUpdate from "./ModalUpdate.react"
 import Loader from "react-spinners/ScaleLoader"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import {CardDescriptionLabel} from '../Common/Card';
+import Typography from '@material-ui/core/Typography';
 
 class List extends React.Component {
 
@@ -56,38 +60,40 @@ class List extends React.Component {
       packages = application.packages ? application.packages : []
 
       if (_.isEmpty(packages)) {
-        entries = <div className="emptyBox">This application does not have any package yet</div>
+        entries = <CardDescriptionLabel>This application does not have any package yet</CardDescriptionLabel>
       } else {
         entries = _.map(packages, (packageItem, i) => {
           return <Item key={"packageItemID_" + packageItem.id} packageItem={packageItem} channels={channels} handleUpdatePackage={this.openUpdatePackageModal} />
         })
       }
     } else {
-      entries = <div className="icon-loading-container"><Loader color="#00AEEF" size="35px" margin="2px"/></div>
+      entries = <Loader color="#00AEEF" size="35px" margin="2px"/>
     }
 
     const packageToUpdate =  !_.isEmpty(packages) && this.state.updatePackageIDModal ? _.findWhere(packages, {id: this.state.updatePackageIDModal}) : null
 
     return (
-      <div>
-        <Row>
-          <Col xs={12}>
-            <h1 className="displayInline mainTitle">Packages</h1>
-            <ModalButton icon="plus" modalToOpen="AddPackageModal"
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant="h4" className="displayInline mainTitle">Packages</Typography>
+          <ModalButton icon="plus" modalToOpen="AddPackageModal"
             data={{channels: channels, appID: this.props.appID}} />
-          </Col>
-        </Row>
-        <div className="groups--packagesList">
-          {entries}
-        </div>
-        {/* Update package modal */}
-        {packageToUpdate &&
-          <ModalUpdate
-            data={{channels: channels, channel: packageToUpdate}}
-            modalVisible={this.state.updatePackageModalVisible}
-            onHide={this.closeUpdatePackageModal} />
-        }
-      </div>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent className="groups--packagesList">
+              {entries}
+              {/* Update package modal */}
+              {packageToUpdate &&
+                <ModalUpdate
+                  data={{channels: channels, channel: packageToUpdate}}
+                  modalVisible={this.state.updatePackageModalVisible}
+                  onHide={this.closeUpdatePackageModal} />
+              }
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     )
   }
 
