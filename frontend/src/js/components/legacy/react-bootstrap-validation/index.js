791b963d55c75136ffe995f5519fa8af90c1dfc7
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-27 16:41:55 +0200

    frontend: Remove react-bootstrap-validation
    This legacy module was still being used with the Bootstrap based forms,
    but all of those have been replaced by Material UI + Formik + Yup.

--- frontend/src/js/components/legacy/react-bootstrap-validation/index.js
+++
@@ -1,8 +0,0 @@
-import Form from './Form';
-import ValidatedInput from './ValidatedInput';
-import Radio from './Radio';
-import RadioGroup from './RadioGroup';
-import Validator from './Validator';
-import FileValidator from './FileValidator';
-
-export { Form, ValidatedInput, Radio, RadioGroup, Validator, FileValidator };
