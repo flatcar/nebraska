838da3faebe2e52535bff582c6c4749101d86f76
Joaquim Rocha <joaquim@kinvolk.io>
2019-08-29 22:15:31 +0200

    frontend: Add main container

--- frontend/src/js/components/Main.react.js
+++ frontend/src/js/components/Main.react.js
@@ -5,6 +5,7 @@ import ProgressBar from "./ProgressBar.react"
 import MainLayout from "./Layouts/MainLayout.react"
 import ApplicationLayout from "./Layouts/ApplicationLayout.react"
 import GroupLayout from "./Layouts/GroupLayout.react"
+import Container from '@material-ui/core/Container';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 
@@ -26,12 +27,14 @@ export default function Main() {
     <ThemeProvider theme={nebraskaTheme}>
       <Header />
       <ProgressBar name="main_progress_bar" color="#ddd" width={0.2} />
-      <Switch>
-        <Route path='/' exact component={MainLayout} />
-        <Route path='/apps' exact component={MainLayout} />
-        <Route path="/apps/:appID" exact component={ApplicationLayout} />
-        <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
-      </Switch>
+      <Container component="main">
+        <Switch>
+          <Route path='/' exact component={MainLayout} />
+          <Route path='/apps' exact component={MainLayout} />
+          <Route path="/apps/:appID" exact component={ApplicationLayout} />
+          <Route path="/apps/:appID/groups/:groupID" component={GroupLayout}/>
+        </Switch>
+      </Container>
     </ThemeProvider>
   );
 }
