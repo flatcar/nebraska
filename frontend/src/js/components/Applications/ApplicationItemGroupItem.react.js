a019538da90e12326ccda62a5c49b2ddece4c02a
Joaquim Rocha <joaquim@kinvolk.io>
2019-09-06 14:32:29 +0200

    frontend: Increase ApplicationItemGroupItem link size

--- frontend/src/js/components/Applications/ApplicationItemGroupItem.react.js
+++ frontend/src/js/components/Applications/ApplicationItemGroupItem.react.js
@@ -1,13 +1,22 @@
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+  groupLink: {
+    fontSize: '1rem'
+  },
+}));
+
 function ApplicationItemGroupItem(props) {
+  const classes = useStyles();
   const instances_total = props.group.instances_stats.total ? '(' + props.group.instances_stats.total + ')' : '';
 
   return(
     <Link
+      className={classes.groupLink}
       to={{pathname: `/apps/${props.group.application_id}/groups/${props.group.id}`}}
       component={RouterLink}
     >
