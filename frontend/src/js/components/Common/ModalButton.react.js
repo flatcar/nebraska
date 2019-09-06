e93b0a6af42159df88000e61c14bb66626c7245b
Joaquim Rocha <joaquim@kinvolk.io>
2019-09-06 17:31:01 +0200

    frontend: Refactor ModalButton
    Make it a function component instead of a class one, and remove unused
    "icon" property.

--- frontend/src/js/components/Common/ModalButton.react.js
+++ frontend/src/js/components/Common/ModalButton.react.js
@@ -1,67 +1,59 @@
-import PropTypes from 'prop-types';
-import React from "react"
-import ApplicationEditDialog from "../Applications/EditDialog"
-import GroupEditDialog from "../Groups/EditDialog"
-import ChannelEditDialog from "../Channels/EditDialog"
-import PackageEditDialog from '../Packages/EditDialog';
-import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import PropTypes from 'prop-types';
+import React from 'react';
+import ApplicationEditDialog from '../Applications/EditDialog';
+import ChannelEditDialog from '../Channels/EditDialog';
+import GroupEditDialog from '../Groups/EditDialog';
+import PackageEditDialog from '../Packages/EditDialog';
 
-class ModalButton extends React.Component {
+function ModalButton(props) {
+  let [showModal, setShowModal] = React.useState(false);
 
-  constructor(props) {
-    super(props)
-    this.close = this.close.bind(this)
-    this.open = this.open.bind(this)
-
-    this.state = {showModal: false}
+  function close() {
+    setShowModal(false);
   }
 
-  close() {
-    this.setState({showModal: false})
+  function open() {
+    setShowModal(true);
   }
 
-  open() {
-    this.setState({showModal: true})
+  let options = {
+    create: true,
+    show: showModal,
+    data: props.data,
+    onHide: close,
   }
 
-  render() {
-    var options = {
-      show: this.state.showModal,
-      data: this.props.data
-    }
-
-    switch (this.props.modalToOpen) {
-      case "AddApplicationModal":
-        var modal = <ApplicationEditDialog create {...options} onHide={this.close} />
-        break
-      case "AddGroupModal":
-        var modal = <GroupEditDialog create {...options} onHide={this.close} />
-        break
-      case "AddChannelModal":
-        var modal = <ChannelEditDialog create {...options} onHide={this.close} />
-        break
-      case "AddPackageModal":
-        var modal = <PackageEditDialog create {...options} onHide={this.close} />
-        break
-    }
-
-    // @todo: verify whether aria-label should be more specific (in which
-    // case it should be set from the caller).
-    return(
-      <div>
-        <Fab size="small" aria-label="add" onClick={this.open.bind()}>
-          <AddIcon />
-        </Fab>
-        {modal}
-      </div>
-    )
+  let modal = null;
+  switch (props.modalToOpen) {
+    case 'AddApplicationModal':
+      modal = <ApplicationEditDialog {...options} />;
+      break;
+    case 'AddGroupModal':
+      modal = <GroupEditDialog {...options} />;
+      break;
+    case 'AddChannelModal':
+      modal = <ChannelEditDialog {...options} />;
+      break;
+    case 'AddPackageModal':
+      modal = <PackageEditDialog {...options} />;
+      break;
   }
 
+  // @todo: verify whether aria-label should be more specific (in which
+  // case it should be set from the caller).
+  return(
+    <div>
+      <Fab size="small" aria-label="add" onClick={open}>
+        <AddIcon />
+      </Fab>
+      {modal}
+    </div>
+  );
 }
 
 ModalButton.propTypes = {
-  icon: PropTypes.string.isRequired,
   modalToOpen: PropTypes.string.isRequired,
   data: PropTypes.object
 }
