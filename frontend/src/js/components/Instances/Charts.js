676a953bd167123129a88288cca590fcc74f89e2
Joaquim Rocha <joaquim@kinvolk.io>
2019-10-08 11:53:37 +0200

    frontend: Add one decimal place to the ProgressDoughnut label
    The ProgressDoughnut label was rounding the percentage to be displayed
    with no decimal places, so in cases where the instance count for a
    certain status was very low (< 1%), the chart would show a percentage of
    0, even if that was not the case.
    
    This patch fixes that by rounding with one decimal place.

--- frontend/src/js/components/Instances/Charts.js
+++ frontend/src/js/components/Instances/Charts.js
@@ -93,7 +93,7 @@ function ProgressDoughnut(props) {
                 <VictoryLabel
                   textAnchor="middle" verticalAnchor="middle"
                   x={width / 2} y={height / 2}
-                  text={`${Math.round(value)}%`}
+                  text={`${value.toFixed(1)}%`}
                   style={{
                     fontSize: `${radius * .25}px`,
                     fontFamily: theme.typography.fontFamily,
