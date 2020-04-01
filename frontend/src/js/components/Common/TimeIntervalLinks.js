281aafe8af78137b2aa499b66ac5f1bbaeaa4cc9
Ashu Ghildiyal <ashu@kinvolk.io>
2020-04-01 17:43:33 +0530

    frontend: Add TimeInterval Component that has duration links.
    VersionBreakdownChart and StatusBreakdownChart needed a link for
    duration and as we will also need it for the Update Progress Chart so
    this patch makes it a component which renders durationLinks based on
    input provided by timeInterval constants.

---
+++ frontend/src/js/components/Common/TimeIntervalLinks.js
@@ -0,0 +1,32 @@
+import { Grid,Link, makeStyles,Typography } from '@material-ui/core';
+import React from 'react';
+import timeIntervals from '../../constants/timeInterval';
+
+const useStyles = makeStyles(theme => ({
+  title:{
+    fontSize:'1rem'
+  }
+}));
+
+export default function TimeIntervalLinks(props){
+  const [activeLink,setActiveLink]=React.useState(timeIntervals[0]);
+  const classes=useStyles();
+  function handleIntervalSelect(link){
+    setActiveLink(link);
+    props.intervalChangeHandler(link);
+  }
+  return (
+    <Grid container spacing={1}>
+      {
+           timeIntervals.map((link)=> <Grid key={link} item onClick={(e)=>handleIntervalSelect(link)}>
+             <Link underline="none" component="button" color={link===activeLink?'inherit':'primary'}>
+               <Typography className={classes.title}>
+                 {link}
+               </Typography>
+             </Link></Grid>)
+       }
+
+    </Grid>
+  );
+}
+
