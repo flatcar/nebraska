a2340712de7be4d058455d1e182a9a3ad708cf83
Ashu Ghildiyal <ashu@kinvolk.io>
2020-06-25 14:14:29 +0530

    frontend: Update tests
    With new designs our tests are now outdated and require modifications
    at some places. Thus this patch updates test to work with the new design
    changes.

--- frontend/src/js/__tests__/Applications/Item.spec.js
+++ frontend/src/js/__tests__/Applications/Item.spec.js
@@ -25,18 +25,20 @@ describe('Application Item', () => {
       },
       'id': '123',
       'name': 'ABC',
-      'channels': [{
-        'id': 'DEF',
-        'name': 'main',
-        'color': '#777777',
-        'created_ts': '2018-10-16T21:07:56.819939+05:30',
-        'application_id': '123',
-        'package_id': 'XYZ',
-        'package': {'id': 'PACK_ID', 'type': 4,
-                    'version': '1.11.3', 'url': 'https://github.com/kinvolk',
-                    'filename': '', 'description': '', 'size': '', 'hash': '', 'created_ts': '2019-07-18T20:10:39.163326+05:30',
-                    'channels_blacklist': null, 'application_id': 'df1c8bbb-f525-49ee-8c94-3ca548b42059', 'flatcar_action': null, 'arch': 0},
-        'arch': 0}],
+      group: {
+        channel: {
+          'id': 'DEF',
+          'name': 'main',
+          'color': '#777777',
+          'created_ts': '2018-10-16T21:07:56.819939+05:30',
+          'application_id': '123',
+          'package_id': 'XYZ',
+          'package': {'id': 'PACK_ID', 'type': 4,
+                      'version': '1.11.3', 'url': 'https://github.com/kinvolk',
+                      'filename': '', 'description': '', 'size': '', 'hash': '', 'created_ts': '2019-07-18T20:10:39.163326+05:30',
+                      'channels_blacklist': null, 'application_id': 'df1c8bbb-f525-49ee-8c94-3ca548b42059', 'flatcar_action': null, 'arch': 0},
+          'arch': 0}
+      },
       'description': 'App Item Description'
     };
     const {getByText} = render(<BrowserRouter>
@@ -46,7 +48,7 @@ describe('Application Item', () => {
     </BrowserRouter>);
     expect(getByText(minProps.description)).toBeInTheDocument();
     expect(getByText(minProps.instances.count)).toBeInTheDocument();
-    expect(getByText(minProps.channels[0].name)).toBeInTheDocument();
+    expect(getByText(minProps.name)).toBeInTheDocument();
     expect(getByText(minProps.id)).toBeInTheDocument();
   });
 });
