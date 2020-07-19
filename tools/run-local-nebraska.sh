6b9f7222977f05bb134d5be23080d1c793001ef0
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-07-19 13:45:45 +0200

    tools: Add a script to run a local nebraska instance
    The script is meant for development only.

---
+++ tools/run-local-nebraska.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script runs a local instance of nebraska. It expects that both
+# frontend and backend are already built, and the database in expected
+# is already running. To override the database location nebraska is
+# using, specify the NEBRASKA_DB_URL environment variable.
+#
+# Whatever parameters passed to the script are forwarded to nebraska.
+
+set -euo pipefail
+
+tools_dir="$(dirname "${0}")"
+binary="${tools_dir}/../bin/nebraska"
+static_dir="${tools_dir}/../frontend/build"
+
+LOGXI=* \
+"${binary}" \
+    -auth-mode noop \
+    -http-log \
+    -http-static-dir "${static_dir}" \
+    "${@}"
