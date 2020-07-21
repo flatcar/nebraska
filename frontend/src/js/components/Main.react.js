5098fc5d33fea5e4ffed46522f97ef4b2a998794
Ashu Ghildiyal <ashu@kinvolk.io>
2020-07-21 16:44:14 +0530

    frontend: Add nebraska version in UI
    With the new design changes we now show nebraska version in UI as well.
    Thus this patch adds the version to UI.

--- frontend/src/js/components/Main.react.js
+++ frontend/src/js/components/Main.react.js
@@ -5,6 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import {Route, Switch } from 'react-router-dom';
+import Footer from './Footer';
 import Header from './Header.react';
 import ApplicationLayout from './Layouts/ApplicationLayout.react';
 import GroupLayout from './Layouts/GroupLayout.react';
@@ -60,6 +61,7 @@ export default function Main() {
           <Route path="/apps/:appID/groups/:groupID/instances" exact component={InstanceListLayout} />
           <Route path="/apps/:appID/groups/:groupID/instances/:instanceID" exact component={InstanceLayout} />
         </Switch>
+        <Footer/>
       </Container>
     </ThemeProvider>
   );
