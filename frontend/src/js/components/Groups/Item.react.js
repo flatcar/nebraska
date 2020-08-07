80610801a14f5fefa15a76fba790582f9e7808a6
Joaquim Rocha <joaquim@kinvolk.io>
2020-08-07 11:37:50 +0100

    frontend: Simplify the groups' instances count time period label
    The mentioned label was showing "in last 24 hours" but this sounds a bit
    quirky. It should either be "in the last 24 hours" or "last 24 hours".
    This patch changes it to the latter, in order to simplify/clarify the
    message.

--- frontend/src/js/components/Groups/Item.react.js
+++ frontend/src/js/components/Groups/Item.react.js
@@ -109,7 +109,7 @@ function Item(props) {
                   <ScheduleIcon color="disabled"/>
                   <Box pl={1} color="text.disabled">
                     <Typography variant="subtitle1">
-                      {'in last 24 hours'}
+                      last 24 hours
                     </Typography>
                   </Box>
                 </Box>
