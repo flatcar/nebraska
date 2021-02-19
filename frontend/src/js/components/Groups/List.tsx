e359768092cee563d6ad2fe445142af873edc78c
Ashu Ghildiyal <ashu@kinvolk.io>
2021-02-19 16:39:30 +0530

    frontend: Port Groups directory

--- frontend/src/js/components/Groups/List.tsx
+++ frontend/src/js/components/Groups/List.tsx
@@ -1,20 +1,18 @@
 import { withStyles } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
 import MuiList from '@material-ui/core/List';
 import Paper from '@material-ui/core/Paper';
-import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'underscore';
+import { Channel, Group} from '../../api/apiDataTypes';
 import { applicationsStore } from '../../stores/Stores';
 import Empty from '../Common/EmptyContent';
 import ListHeader from '../Common/ListHeader';
-import SearchInput from '../Common/ListSearch';
 import Loader from '../Common/Loader';
 import ModalButton from '../Common/ModalButton';
 import EditDialog from './EditDialog';
-import Item from './Item.react';
+import Item from './Item';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     '& > hr:first-child': {
       display: 'none'
@@ -22,144 +20,121 @@ const styles = theme => ({
   }
 });
 
-class List extends React.Component {
+function List(props: {appID: string; classes: Record<'root', string>}) {
+  const [application, setApplication] =
+  React.useState(applicationsStore.getCachedApplication(props.appID));
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [updateGroupModalVisible, setUpdateGroupModalVisible] = React.useState(false);
+  const [updateGroupIDModal, setUpdateGroupIDModal] = React.useState<string | null>(null);
+  const [updateAppIDModal, setUpdateAppIDModal] = React.useState<string | null>(null);
 
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-    this.searchUpdated = this.searchUpdated.bind(this);
-    this.openUpdateGroupModal = this.openUpdateGroupModal.bind(this);
-    this.closeUpdateGroupModal = this.closeUpdateGroupModal.bind(this);
+  function closeUpdateGroupModal() {
+    setUpdateGroupModalVisible(false);
+  }
 
-    this.state = {
-      application: applicationsStore.getCachedApplication(this.props.appID),
-      searchTerm: '',
-      updateGroupModalVisible: false,
-      updateGroupIDModal: null,
-      updateAppIDModal: null
+  function openUpdateGroupModal(appID: string, groupID: string) {
+    setUpdateGroupModalVisible(true);
+    setUpdateGroupIDModal(groupID);
+    setUpdateAppIDModal(appID);
+  }
+
+  React.useEffect(() => {
+    applicationsStore.addChangeListener(onChange);
+    return () => {
+      applicationsStore.removeChangeListener(onChange);
     };
+  },
+  []);
+
+  function onChange() {
+    setApplication(applicationsStore.getCachedApplication(props.appID));
   }
 
-  closeUpdateGroupModal() {
-    this.setState({updateGroupModalVisible: false});
+  function searchUpdated(event: React.ChangeEvent<{value: any}>) {
+    const {value} = event.currentTarget;
+    setSearchTerm(value.toLowerCase());
   }
 
-  openUpdateGroupModal(appID, groupID) {
-    this.setState({
-      updateGroupModalVisible: true,
-      updateGroupIDModal: groupID,
-      updateAppIDModal: appID
-    });
-  }
+  let channels: Channel[] = [];
+  let groups: Group[] = [];
+  let packages = [];
+  let instances = 0;
+  let name = '';
+  let entries: React.ReactNode = '';
 
-  componentDidMount() {
-    applicationsStore.addChangeListener(this.onChange);
-  }
+  if (application) {
+    name = application.name;
+    groups = application.groups ? application.groups : [];
+    packages = application.packages ? application.packages : [];
+    instances = application.instances ? application.instances : [];
+    channels = application.channels ? application.channels : [];
 
-  componentWillUnmount() {
-    applicationsStore.removeChangeListener(this.onChange);
-  }
-
-  onChange() {
-    this.setState({
-      application: applicationsStore.getCachedApplication(this.props.appID)
-    });
-  }
-
-  searchUpdated(event) {
-    const {name, value} = event.currentTarget;
-    this.setState({searchTerm: value.toLowerCase()});
-  }
-
-  render() {
-    const application = this.state.application;
-
-    let channels = [];
-    let groups = [];
-    let packages = [];
-    let instances = 0;
-    let name = '';
-    let entries = '';
-
-    if (application) {
-      name = application.name;
-      groups = application.groups ? application.groups : [];
-      packages = application.packages ? application.packages : [];
-      instances = application.instances ? application.instances : [];
-      channels = application.channels ? application.channels : [];
-
-      if (this.state.searchTerm) {
-        groups = groups.filter(app => app.name.toLowerCase().includes(this.state.searchTerm));
-      }
-
-      if (_.isEmpty(groups)) {
-        if (this.state.searchTerm) {
-          entries = <Empty>No results found.</Empty>;
-        } else {
-          entries = <Empty>
-            There are no groups for this application yet.
-            <br/><br/>
-            Groups help you control how you want to distribute updates to
-            a specific set of instances.
-          </Empty>;
-        }
-      } else {
-        entries = _.map(groups, (group, i) => {
-          return (
-            <Item
-              key={'groupID_' + group.id}
-              group={group}
-              appName={name}
-              channels={channels}
-              handleUpdateGroup={this.openUpdateGroupModal}
-            />);
-        });
-      }
-
-    } else {
-      entries = <Loader />;
+    if (searchTerm) {
+      groups = groups.filter((app: Group) => app.name.toLowerCase().includes(searchTerm));
     }
 
-    const groupToUpdate = !_.isEmpty(groups) &&
-      this.state.updateGroupIDModal ?
-      _.findWhere(groups, {id: this.state.updateGroupIDModal}) : null;
-    const {classes} = this.props;
+    if (_.isEmpty(groups)) {
+      if (searchTerm) {
+        entries = <Empty>No results found.</Empty>;
+      } else {
+        entries = <Empty>
+          There are no groups for this application yet.
+          <br/><br/>
+          Groups help you control how you want to distribute updates to
+          a specific set of instances.
+        </Empty>;
+      }
+    } else {
+      entries = _.map(groups, (group, i) => {
+        return (
+          <Item
+            key={'groupID_' + group.id}
+            group={group}
+            appName={name}
+            channels={channels}
+            handleUpdateGroup={openUpdateGroupModal}
+          />);
+      });
+    }
 
-    return (
-      <>
-        <ListHeader
-          title="Groups"
-          actions={[
-            <ModalButton
-              icon="plus"
-              modalToOpen="AddGroupModal"
-              data={{
-                channels: channels,
-                appID: this.props.appID
-              }}
-            />
-          ]}
-        />
-        <Paper>
-          <MuiList className={classes.root}>
-            {entries}
-          </MuiList>
-          {groupToUpdate &&
-          <EditDialog
-            data={{group: groupToUpdate, channels: channels}}
-            show={this.state.updateGroupModalVisible}
-            onHide={this.closeUpdateGroupModal}
-          />
-          }
-        </Paper>
-      </>
-    );
+  } else {
+    entries = <Loader />;
   }
 
-}
+  const groupToUpdate = !_.isEmpty(groups) &&
+      updateGroupIDModal ?
+    _.findWhere(groups, {id: updateGroupIDModal}) : null;
+  const {classes} = props;
 
-List.propTypes = {
-  appID: PropTypes.string.isRequired
-};
+  return (
+    <>
+      <ListHeader
+        title="Groups"
+        actions={[
+          <ModalButton
+            icon="plus"
+            modalToOpen="AddGroupModal"
+            data={{
+              channels: channels,
+              appID: props.appID
+            }}
+          />
+        ]}
+      />
+      <Paper>
+        <MuiList className={classes.root}>
+          {entries}
+        </MuiList>
+        {groupToUpdate &&
+          <EditDialog
+            data={{group: groupToUpdate, channels: channels}}
+            show={updateGroupModalVisible}
+            onHide={closeUpdateGroupModal}
+          />
+        }
+      </Paper>
+    </>
+  );
+}
 
 export default withStyles(styles)(List);
