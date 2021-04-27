cd0b3fbc4a236c76b8e3336701534df010e6c838
Ashu Ghildiyal <ashu@kinvolk.io>
2021-04-27 17:39:45 +0530

    frontend: Start the auth flow with oidc-react package and use env vars

--- frontend/src/js/components/Main..tsx
+++ frontend/src/js/components/Main..tsx
@@ -4,7 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { AuthProvider, useAuth } from 'oidc-react';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import ApplicationLayout from './Layouts/ApplicationLayout';
@@ -12,6 +13,7 @@ import GroupLayout from './Layouts/GroupLayout';
 import InstanceLayout from './Layouts/InstanceLayout';
 import InstanceListLayout from './Layouts/InstanceListLayout';
 import MainLayout from './Layouts/MainLayout';
+import Loader from './Common/Loader';
 
 declare module '@material-ui/core/styles/createPalette' {
   interface Palette {
@@ -48,30 +50,63 @@ const nebraskaTheme = createMuiTheme({
   },
 });
 
+const oidcConfig = {
+  onSignIn: async () => {
+    window.location.hash = '';
+  },
+  authority: process.env.REACT_APP_AUTH_AUTHORITY,
+  clientId: process.env.REACT_APP_AUTH_CLIENT_ID,
+  redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI,
+};
+
+function RedirectRoute() {
+  const history = useHistory();
+  React.useEffect(() => {
+    history.replace('/');
+  }, []);
+
+  return <div>Logging You In....</div>;
+}
+
+function AppRoutes() {
+  const auth = useAuth();
+
+  if (auth?.userData?.id_token) {
+    localStorage.setItem('nebraska_auth_token', auth.userData.id_token);
+    return (
+      <ThemeProvider theme={nebraskaTheme}>
+        <CssBaseline />
+        <Header />
+        <Container component="main">
+          <Switch>
+            <Route path="/" exact component={MainLayout} />
+            <Route path="/apps" exact component={MainLayout} />
+            <Route path="/apps/:appID" exact component={ApplicationLayout} />
+            <Route path="/apps/:appID/groups/:groupID" exact component={GroupLayout} />
+            <Route
+              path="/apps/:appID/groups/:groupID/instances"
+              exact
+              component={InstanceListLayout}
+            />
+            <Route
+              path="/apps/:appID/groups/:groupID/instances/:instanceID"
+              exact
+              component={InstanceLayout}
+            />
+            <Route path="*" component={RedirectRoute} />
+          </Switch>
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    );
+  }
+  return <Loader />;
+}
+
 export default function Main() {
   return (
-    <ThemeProvider theme={nebraskaTheme}>
-      <CssBaseline />
-      <Header />
-      <Container component="main">
-        <Switch>
-          <Route path="/" exact component={MainLayout} />
-          <Route path="/apps" exact component={MainLayout} />
-          <Route path="/apps/:appID" exact component={ApplicationLayout} />
-          <Route path="/apps/:appID/groups/:groupID" exact component={GroupLayout} />
-          <Route
-            path="/apps/:appID/groups/:groupID/instances"
-            exact
-            component={InstanceListLayout}
-          />
-          <Route
-            path="/apps/:appID/groups/:groupID/instances/:instanceID"
-            exact
-            component={InstanceLayout}
-          />
-        </Switch>
-        <Footer />
-      </Container>
-    </ThemeProvider>
+    <AuthProvider {...oidcConfig}>
+      <AppRoutes />
+    </AuthProvider>
   );
 }
