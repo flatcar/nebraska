bd6c158a1a7c7852ef1bb2713551103928929e70
Santhosh Nagaraj S <santhosh@kinvolk.io>
2021-08-27 15:08:54 +0530

    updater: Add context to updateHandler functions
    Signed-off-by: Santhosh Nagaraj S <santhosh@kinvolk.io>

--- updater/updater.go
+++ updater/updater.go
@@ -7,9 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/url"
-	"os"
 
 	"github.com/google/uuid"
 	retryablehttp "github.com/hashicorp/go-retryablehttp"
@@ -75,69 +73,6 @@ type Updater struct {
 	httpClient *retryablehttp.Client
 }
 
-type UpdateHandler interface {
-	FetchUpdate(info *UpdateInfo) error
-	ApplyUpdate(info *UpdateInfo) error
-}
-
-type UpdateInfo struct {
-	HasUpdate bool
-	omahaResp *omaha.Response
-}
-
-func (u *UpdateInfo) GetVersion() string {
-	app := u.getApp()
-	if app != nil && app.UpdateCheck != nil {
-		return app.UpdateCheck.Manifest.Version
-	}
-
-	return ""
-}
-
-func (u *UpdateInfo) GetURLs() []string {
-	app := u.getApp()
-	if app != nil && app.UpdateCheck != nil {
-		omahaURLs := app.UpdateCheck.URLs
-		urls := make([]string, len(omahaURLs))
-		for i, url := range omahaURLs {
-			urls[i] = url.CodeBase
-		}
-
-		return urls
-	}
-
-	return nil
-}
-
-func (u *UpdateInfo) GetURL() string {
-	if urls := u.GetURLs(); urls != nil {
-		return urls[0]
-	}
-
-	return ""
-}
-
-func (u *UpdateInfo) GetUpdateStatus() string {
-	app := u.getApp()
-	if app != nil && app.UpdateCheck != nil {
-		return string(app.UpdateCheck.Status)
-	}
-
-	return ""
-}
-
-func (u *UpdateInfo) GetOmahaResponse() *omaha.Response {
-	return u.omahaResp
-}
-
-func (u *UpdateInfo) getApp() *omaha.AppResponse {
-	if u.omahaResp != nil {
-		return u.omahaResp.Apps[0]
-	}
-
-	return nil
-}
-
 func New(omahaURL string, instanceID string, instanceVersion string, appID string, channel string) (*Updater, error) {
 	return NewWithHttpClient(omahaURL, instanceID, instanceVersion, appID, channel, retryablehttp.NewClient())
 }
@@ -190,7 +125,6 @@ func (u *Updater) SendOmahaRequest(url string, req *omaha.Request) (*omaha.Respo
 	contentType := resp.Header.Get("Content-Type")
 	omahaResp, err := omaha.ParseResponse(contentType, respBody)
 	if err != nil {
-		// TODO: Convert to omaha error response
 		return nil, err
 	}
 	return omahaResp, nil
@@ -202,8 +136,6 @@ func (u *Updater) CheckForUpdates(ctx context.Context) (*UpdateInfo, error) {
 	app.AddUpdateCheck()
 
 	resp, err := u.SendOmahaRequest(u.omahaURL, req)
-	requestByte, _ := xml.Marshal(resp)
-	fmt.Println(string(requestByte))
 	if err != nil {
 		return nil, err
 	}
@@ -257,7 +189,7 @@ func (u *Updater) TryUpdate(ctx context.Context, handler UpdateHandler) error {
 	}
 
 	// Fetch update
-	err = handler.FetchUpdate(info)
+	err = handler.FetchUpdate(ctx, info)
 	if err != nil {
 		err := u.ReportProgress(ctx, ProgressError)
 		if err != nil {
@@ -270,7 +202,7 @@ func (u *Updater) TryUpdate(ctx context.Context, handler UpdateHandler) error {
 		return err
 	}
 
-	err = handler.ApplyUpdate(info)
+	err = handler.ApplyUpdate(ctx, info)
 	if err != nil {
 		err := u.ReportProgress(ctx, ProgressError)
 		if err != nil {
@@ -292,33 +224,4 @@ func (u *Updater) TryUpdate(ctx context.Context, handler UpdateHandler) error {
 	}
 
 	return nil
-}
-
-func main() {
-
-	omahaURL := "http://localhost:8000/v1/update/"
-
-	appID := "4f101e1e-7fca-4a45-a222-c2aa2fd78d84"
-
-	instanceID := "Test8cd4-a18e-466c-b8e1-431b849bfb4c"
-	instanceVersion := "0.2.0"
-
-	versionOverride := os.Getenv("UPDATER_INSTANCE_VERSION")
-	if versionOverride != "" {
-		instanceVersion = versionOverride
-	}
-
-	emptyHandler := NewEmptyHandler()
-
-	updater, err := New(omahaURL, instanceID, instanceVersion, appID, "stable")
-
-	if err != nil {
-		log.Fatal("up err", err)
-	}
-
-	ctx := context.TODO()
-
-	err = updater.TryUpdate(ctx, emptyHandler)
-	fmt.Println("Err", err)
-
 }
