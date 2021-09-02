ed8f2db565aafe3290271b09ec7914d79e5152ff
Santhosh Nagaraj S <santhosh@kinvolk.io>
2021-09-02 17:32:23 +0530

    updater: Remove emptyHandler
    This patch removes the emptyHandler which was
    previously introduced as a reference.
    
    Signed-off-by: Santhosh Nagaraj S <santhosh@kinvolk.io>

--- updater/handler.go
+++ updater/handler.go
@@ -2,29 +2,9 @@ package updater
 
 import (
 	"context"
-	"fmt"
 )
 
 type UpdateHandler interface {
 	FetchUpdate(ctx context.Context, info *UpdateInfo) error
 	ApplyUpdate(ctx context.Context, info *UpdateInfo) error
-}
-
-type emptyHandler struct {
-}
-
-func NewEmptyHandler() UpdateHandler {
-	return emptyHandler{}
-}
-
-func (e emptyHandler) FetchUpdate(ctx context.Context, info *UpdateInfo) error {
-	fmt.Println("Downloading the upload payload:")
-	fmt.Printf("URL: %v\n", info.GetURL())
-	fmt.Printf("Version: %v\n", info.GetVersion())
-	return nil
-}
-
-func (e emptyHandler) ApplyUpdate(ctx context.Context, info *UpdateInfo) error {
-	fmt.Println("Installing the update...")
-	return nil
 }
