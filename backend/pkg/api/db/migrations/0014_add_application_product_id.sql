c708f9772501a76742cca50771c69067066af0b8
Joaquim Rocha <joaquim@kinvolk.io>
2021-09-02 13:55:36 +0100

    backend: Allow the use of a product-id in applications
    A product-id in the form of an app name including a reverse domain is
    allowed since omaha version 3, and it's a human-readable name, making
    the use of this ID much easier than the UUID based one.
    
    This patch allows both the new product-id and the UUID to be used when
    identifying applications.

---
+++ backend/pkg/api/db/migrations/0014_add_application_product_id.sql
@@ -0,0 +1,9 @@
+-- +migrate Up
+
+alter table application add column product_id varchar(100) default null;
+alter table application add constraint application_unique_product_id unique(product_id);
+
+-- +migrate Down
+
+alter table application drop constraint application_unique_product_id;
+alter table application drop column product_id;
