4ea270c925c1ac82d981966e235c86ec3b592bb1
René Dudfield <rene@kinvolk.io>
2021-09-17 13:21:33 +0200

    backend: Add packages checkPackage function for validation
    This centralizes some package validation that is done.

--- backend/pkg/api/actions_test.go
+++ backend/pkg/api/actions_test.go
@@ -13,7 +13,7 @@ func TestAddFlatcarAction(t *testing.T) {
 
 	tTeam, _ := a.AddTeam(&Team{Name: "test_team"})
 	tApp, _ := a.AddApp(&Application{Name: "test_app", TeamID: tTeam.ID})
-	tPkg, _ := a.AddPackage(&Package{Type: PkgTypeFlatcar, URL: null.StringFrom("http://sample.url/pkg"), Version: "12.1.0", ApplicationID: tApp.ID})
+	tPkg, _ := a.AddPackage(&Package{Type: PkgTypeFlatcar, URL: null.StringFrom("http://sample.url/pkg"), Version: "12.1.0", ApplicationID: tApp.ID, Filename: null.StringFrom("fname.txt"), Hash: null.StringFrom("2222222"), Size: null.StringFrom("22")})
 
 	flatcarAction, err := a.AddFlatcarAction(&FlatcarAction{Event: "postinstall", Sha256: "fsdkjjfghsdakjfgaksdjfasd", PackageID: tPkg.ID})
 	assert.NoError(t, err)
