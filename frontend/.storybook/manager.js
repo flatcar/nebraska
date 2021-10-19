49832b62023d9c8a6d94d7ba285bfa38c774faaf
René Dudfield <renedudfield@microsoft.com>
2021-10-19 14:01:46 +0200

    frontend: Add storybook configuration
    from Headlamp.

---
+++ frontend/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import theme from './NebraskaTheme';
+
+addons.setConfig({
+  theme: theme,
+});
---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import themesConf from '../src/lib/themes';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
+
+const darkTheme = themesConf['dark'];
+const lightTheme = themesConf['light'];
+
+const withThemeProvider = (Story, context) => {
+  const backgroundColor = context.globals.backgrounds
+    ? context.globals.backgrounds.value
+    : 'light';
+  const theme = backgroundColor !== 'dark' ? lightTheme : darkTheme;
+
+  const ourThemeProvider = (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  );
+  if (process.env.NODE_ENV !== 'test') {
+    return ourThemeProvider;
+  } else {
+    const generateClassName = (rule, styleSheet) =>
+      `${styleSheet?.options.classNamePrefix}-${rule.key}`;
+
+    return (
+      <StylesProvider generateClassName={generateClassName}>
+        {ourThemeProvider}
+      </StylesProvider>
+    )
+  }
+};
+export const decorators = [withThemeProvider];
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+    },
+  },
+};
+
+export const parameters = {
+  backgrounds: {
+    values: [
+      { name: 'light', value: 'light' },
+      { name: 'dark', value: 'dark' },
+    ],
+  },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
