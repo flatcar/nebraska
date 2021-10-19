5393876ea5beed7b84141c64e9fa028cedf21db3
René Dudfield <renedudfield@microsoft.com>
2021-10-19 13:56:36 +0200

    frontend: Move Main..tsx to Main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import './i18n/config';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router-dom';
-import Main from './components/Main.';
+import Main from './components/Main';
 import store from './stores/redux/store';
 
 var AppRoutes = function () {
