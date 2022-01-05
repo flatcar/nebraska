c93b22e5471c83a6dcd60be0dc0e163cf25e1840
Santhosh Nagaraj S <santhosh@kinvolk.io>
2022-01-05 15:46:49 +0000

    backend: Add e2e tests for activity endpoints
    This patch adds the e2e tests for the endpoints
    related to activity
    
    Signed-off-by: Santhosh Nagaraj S <santhosh@kinvolk.io>

---
+++ backend/test/api/activity_test.go
@@ -0,0 +1,40 @@
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/kinvolk/nebraska/backend/pkg/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListActivity(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		// establish DB connection
+		db := newDBForTest(t)
+		teamID := getTeamID(t, db)
+
+		endTime := time.Now()
+		startTime := time.Now().Add(time.Duration(-1 * 24 * 7 * time.Hour))
+		activitiesDB, err := db.GetActivity(teamID, api.ActivityQueryParams{Start: startTime, End: endTime})
+		require.NoError(t, err)
+		require.NotNil(t, activitiesDB)
+
+		// fetch activity from api
+		url := fmt.Sprintf("%s/api/activity?start=%s&end=%s", testServerURL, startTime.Format(time.RFC3339), endTime.Format(time.RFC3339))
+		method := "GET"
+
+		// response
+		var activities []api.Activity
+
+		httpDo(t, url, method, nil, http.StatusOK, "json", &activities)
+
+		assert.Equal(t, len(activitiesDB), len(activities))
+		assert.Equal(t, activitiesDB[0].AppID, activities[0].AppID)
+		assert.Equal(t, activitiesDB[0].GroupID, activities[0].GroupID)
+		assert.Equal(t, activitiesDB[0].GroupName, activities[0].GroupName)
+	})
+}
