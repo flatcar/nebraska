81221305a10bbe14156a3d5dbc8063791daa03c6
Santhosh Nagaraj S <santhosh@kinvolk.io>
2022-01-05 15:38:27 +0000

    backend: Add e2e tests for config endpoint
    This patch adds the e2e tests for config endpoint
    
    Signed-off-by: Santhosh Nagaraj S <santhosh@kinvolk.io>

---
+++ backend/test/api/config_test.go
@@ -0,0 +1,25 @@
+package api_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		url := fmt.Sprintf("%s/config", testServerURL)
+		method := "GET"
+		// response
+		rMap := make(map[string]interface{})
+
+		httpDo(t, url, method, nil, http.StatusOK, "json", &rMap)
+
+		assert.Equal(t, "", rMap["auth_mode"])
+		assert.Equal(t, "", rMap["access_management_url"])
+		assert.Equal(t, "", rMap["login_url"])
+		assert.Equal(t, "", rMap["logout_url"])
+	})
+}
