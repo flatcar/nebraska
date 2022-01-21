a2c72d595c41bb4c7113c77932654e456880e188
Santhosh Nagaraj S <santhosh@kinvolk.io>
2022-01-21 17:00:58 +0530

    backend: Add e2e test for OIDC auth mode
    This patch adds end to end test for OIDC auth mode.
    
    Signed-off-by: Santhosh Nagaraj S <santhosh@kinvolk.io>

--- backend/go.mod
+++ backend/go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/labstack/echo/v4 v4.4.0
 	github.com/labstack/gommon v0.3.0
 	github.com/lib/pq v1.10.1
+	github.com/oauth2-proxy/mockoidc v0.0.0-20210703044157-382d3faf2671 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.10.0
 	github.com/rs/zerolog v1.21.0
---
+++ backend/test/auth/oidc/helper_test.go
@@ -0,0 +1,40 @@
+package auth_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/oauth2-proxy/mockoidc"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kinvolk/nebraska/backend/pkg/api"
+)
+
+// newDBForTest is a helper function that
+// establishes connection with test db and returns the db struct.
+func newDBForTest(t *testing.T) *api.API {
+	t.Helper()
+	db, err := api.NewForTest(api.OptionInitDB)
+	require.NoError(t, err)
+	require.NotNil(t, db)
+	return db
+}
+
+// newOIDCMockServer creates a new mockoidc server and returns it.
+func newOIDCMockServer(t *testing.T) *mockoidc.MockOIDC {
+	t.Helper()
+	m, err := mockoidc.NewServer(nil)
+	require.NoError(t, err)
+	return m
+}
+
+// startOIDCMockServer starts the mockoidc server at 127.0.0.1:8080.
+func startOIDCMockServer(t *testing.T, m *mockoidc.MockOIDC) {
+	t.Helper()
+	// create listener
+	ln, err := net.Listen("tcp", "127.0.0.1:8080")
+	require.NoError(t, err)
+	// start server
+	err = m.Start(ln, nil)
+	require.NoError(t, err)
+}
