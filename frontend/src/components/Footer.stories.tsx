ebf5a4202037386d172e48788ba05245a9d5195c
Joaquim Rocha <joaquim.rocha@microsoft.com>
2022-06-22 21:03:48 +0100

    frontend: Add stories for Footer

---
+++ frontend/src/components/Footer.stories.tsx
@@ -0,0 +1,46 @@
+import { createStore } from '@reduxjs/toolkit';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import FooterComponent from './Footer';
+
+export default {
+  title: 'Footer',
+  component: FooterComponent,
+  argTypes: {},
+  decorators: [
+    Story => {
+      return (
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      );
+    },
+  ],
+} as Meta;
+
+const Template: Story = args => {
+  // eslint-disable-next-line no-unused-vars
+  const store = createStore((state = { config: {} }, action) => state, {
+    config: {
+      ...args,
+    },
+  });
+  return (
+    <Provider store={store}>
+      <FooterComponent />
+    </Provider>
+  );
+};
+
+export const FooterNoOverride = Template.bind({});
+FooterNoOverride.args = {
+  title: '',
+  nebraska_version: '',
+};
+
+export const FooterOverride = Template.bind({});
+FooterOverride.args = {
+  title: 'Some Pro Update Service',
+  nebraska_version: '1.2.3',
+};
