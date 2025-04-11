596e00cfe1d3fc60ca8bcf6166febe6953519786
Ervin Rácz <ervin.racz@protonmail.com>
2025-04-11 12:54:49 +0300

    chore(frontend): update env var definitions & state handling in picker
    - Updated `.env` file to use quotes for environment variable values.
    - Modified `theme.ts` to use `import.meta.env` for accessing environment variables.
    - Improved `AutoCompletePicker` and `TimezonePicker` components by moving `setSelectedValue` and `setSelectedTimezone` logic into `onChange` handlers.
    - Added TypeScript definitions for Vite environment variables in `vite-env.d.ts`.

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PRIMARY_COLOR: string;
+  readonly VITE_PROJECT_NAME: string;
+  readonly VITE_APPBAR_BG: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
